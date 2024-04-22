806a2d37ef8bfcf5b71145dea79cdca67027ba16
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-22 13:51:00 +0400

    Updated Error component

--- src/components/error/ErrorPage.tsx
+++ src/components/error/ErrorPage.tsx
@@ -1,16 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import { IoIosArrowRoundBack } from "react-icons/io";
+
 
 const ErrorPage = () => {
 
     const error: any = useRouteError()
 
     return (
-        <section>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+        <section className="error">
+            <div>
+                <img src="https://25.media.tumblr.com/6f5b26a59c1651737e6df675b5a4c7a9/tumblr_mvf876gg0H1rev2tgo1_500.gif" alt="Not-Found" />
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+                <Link to='/'><IoIosArrowRoundBack /></Link>
+            </div>
         </section>
     );
 }
