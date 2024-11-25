c79bd12abfc4b105ffd5f39b179ebc0da250496a
AzarAhmadov <ehmedovh111@gmail.com>
2024-11-25 23:22:50 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes/routes';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import './assets/css/App.css'
+import { router } from "./routes/routes";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "./assets/css/App.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
