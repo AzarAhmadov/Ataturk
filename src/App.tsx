1b732a6aa3b5366fd5f36be5b73e10e1af179d7a
AzarAhmadov <ehmedovh111@gmail.com>
2024-11-25 23:23:08 +0400

    add ScrollRestoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react"
-import Home from "./pages/HomeScreen/HomeScreen"
+import { FC } from "react";
+import Home from "./pages/HomeScreen/HomeScreen";
+import { ScrollRestoration } from "react-router-dom";
 
 const App: FC = () => {
   return (
     <main>
+      <ScrollRestoration />
       <Home />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
